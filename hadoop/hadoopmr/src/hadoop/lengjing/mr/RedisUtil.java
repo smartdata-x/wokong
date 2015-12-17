228d10fbcb03a656f1d76f45d07f097f84987148
cjyou <cjyou20110920sspu@163.com>
2015-12-17 08:41:01 +0800

    修改RedisUtil文件中redis连接参数为非常量

--- hadoop/hadoopmr/src/hadoop/lengjing/mr/RedisUtil.java
+++ hadoop/hadoopmr/src/hadoop/lengjing/mr/RedisUtil.java
@@ -33,26 +33,26 @@ import redis.clients.jedis.JedisPoolConfig;
 public final class RedisUtil {
 	    
 	    /** Redis服务器IP */
-	    private static String ADDR;
+	    private static String address;
 	    
 	    /** Redis的端口号  */
-	    private static int PORT;
+	    private static int port;
 	    
 	    /** 访问密码  */
-	    private static String AUTH;
+	    private static String auth;
 	    
-	    private static int DATABASE;
+	    private static int database;
 	    
 	    /** 可用连接实例的最大数目，默认值为8,如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。*/
-	    private static int MAX_ACTIVE = 1024;
+	    private static final int MAX_ACTIVE = 1024;
 	    
 	    /** 控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值也是8。 */
-	    private static int MAX_IDLE = 200;
+	    private static final int MAX_IDLE = 200;
 	    
 	    /** 等待可用连接的最大时间，单位毫秒，默认值为-1，表示永不超时。如果超过等待时间，则直接抛出JedisConnectionException； */
-	    private static int MAX_WAIT = 10000;
+	    private static final int MAX_WAIT = 10000;
 	    
-	    private static int TIMEOUT = 10000;
+	    private static final int TIMEOUT = 10000;
 	    
 	    /** 在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的； */
 	    private static boolean TEST_ON_BORROW = true;
@@ -64,10 +64,10 @@ public final class RedisUtil {
         InputStream in = Object.class.getResourceAsStream("/conf.properties");  
          try  {    
             prop.load(in);    
-            ADDR = prop.getProperty("ip").trim();    
-            PORT = Integer.parseInt(prop.getProperty("port").trim());
-            AUTH = prop.getProperty("auth").trim(); 
-            DATABASE = Integer.parseInt(prop.getProperty("database").trim()); 
+            address = prop.getProperty("ip").trim();    
+            port = Integer.parseInt(prop.getProperty("port").trim());
+            auth = prop.getProperty("auth").trim(); 
+            database = Integer.parseInt(prop.getProperty("database").trim()); 
          }catch  (IOException e) {    
             e.printStackTrace();    
         }    
@@ -83,7 +83,7 @@ public final class RedisUtil {
 	            config.setMaxTotal(MAX_ACTIVE);
 	            config.setMaxWaitMillis(MAX_WAIT);
 	            config.setTestOnBorrow(TEST_ON_BORROW);
-	            jedisPool = new JedisPool(config, ADDR, PORT, TIMEOUT, AUTH, DATABASE);
+	            jedisPool = new JedisPool(config, address, port, TIMEOUT, auth, database);
 	        } catch (Exception e) {
 	            e.printStackTrace();
 	        }
