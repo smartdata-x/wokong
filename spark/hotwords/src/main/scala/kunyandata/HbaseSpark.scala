452de19577a30edd010d139c3c7054bae7fa99d3
sunhuijun <sunhuijun@kunyan-inc.com>
2016-02-22 10:22:33 +0800

    修改格式

--- spark/hotwords/src/main/scala/kunyandata/HbaseSpark.scala
+++ spark/hotwords/src/main/scala/kunyandata/HbaseSpark.scala
@@ -1,4 +1,4 @@
-package kunyandata
+﻿package kunyandata
 
 /**
   * Created by lenovo on 2016/2/16.
@@ -21,7 +21,7 @@ import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 
-  object HbaseSpark {
+ object HbaseSpark {
   val mapAfter = new mutable.HashMap[String, Int]()
   val mapBefore = new mutable.HashMap[String, Int]()
   val mapDiff = new mutable.HashMap[String, Int]()
@@ -38,117 +38,127 @@ import scala.io.Source
   //得到hbase里的数据并作为RDD
   def obtainHbaseRdd(): Unit = {
 
-  println("after config")
+    println("after config")
 
-  getTime()
-  init("6_analyzed")
-  val hbaseRdd = sparkContext.newAPIHadoopRDD(conf, classOf[TableInputFormat]
-   , classOf[ImmutableBytesWritable], classOf[Result])
-  init("5_analyzed")
-  val hbaseRdd1 = sparkContext.newAPIHadoopRDD(conf, classOf[TableInputFormat]
-    , classOf[ImmutableBytesWritable], classOf[Result])
-  init("3_analyzed")
-  val hbaseRdd2 = sparkContext.newAPIHadoopRDD(conf, classOf[TableInputFormat]
+    getTime()
+
+    init("6_analyzed")
+    val hbaseRdd = sparkContext.newAPIHadoopRDD(conf, classOf[TableInputFormat]
+     , classOf[ImmutableBytesWritable], classOf[Result])
+
+    init("5_analyzed")
+    val hbaseRdd1 = sparkContext.newAPIHadoopRDD(conf, classOf[TableInputFormat]
+     , classOf[ImmutableBytesWritable], classOf[Result])
+
+    init("3_analyzed")
+    val hbaseRdd2 = sparkContext.newAPIHadoopRDD(conf, classOf[TableInputFormat]
     , classOf[ImmutableBytesWritable], classOf[Result])
 
-  val rdd=hbaseRdd.++(hbaseRdd1)
-  val rdd1=rdd.++(hbaseRdd2)
+    val rdd=hbaseRdd.++(hbaseRdd1)
+    val rdd1=rdd.++(hbaseRdd2)
 
   //排序+topn
-  val array = rdd1.flatMap(SplitWords(_)).map((_, 1)).reduceByKey(_ + _).map(x => (x._2, x._1)).sortByKey(false).top(200)
-  array.foreach(println)
+   val array = rdd1.flatMap(SplitWords(_)).map((_, 1)).reduceByKey(_ + _).map(x => (x._2, x._1)).sortByKey(false).top(200)
+   array.foreach(println)
 
-  println("after hbase")
+   println("after hbase")
 
   //获取当前小时的时间戳 并以此时间戳命名文件夹
-  val dataafter = new Date(new Date().getTime)
-  val sdf = new SimpleDateFormat("yyyy-MM-dd-HH")
-  val strafter = sdf.format(dataafter)
-  println(strafter)
 
-  val fileafter = new File("/home/shj/" + strafter)
+   val dataafter = new Date(new Date().getTime)
+   val sdf = new SimpleDateFormat("yyyy-MM-dd-HH")
+   val strafter = sdf.format(dataafter)
+   println(strafter)
+
+   val fileafter = new File("/home/shj/" + strafter)
 
   //将当前小时的数据写进mapafter集合中 并写进以当前小时时间戳命名的本地文件中
-  var arraya = ""
-  var i = 1
-  for (a <- array) {
+   var arraya = ""
+   var i = 1
+   for (a <- array) {
 
-   arraya += a._2.toString
-   arraya += "\t"
-   arraya += i.toString
-   i += 1
-   arraya += "\n"
+    arraya += a._2.toString
+    arraya += "\t"
+    arraya += i.toString
+    i += 1
+    arraya += "\n"
 
-   val writer = new PrintWriter(fileafter,"UTF-8")
-   writer.write(arraya)
-   writer.flush()
-   writer.close()
+    val writer = new PrintWriter(fileafter,"UTF-8")
+    writer.write(arraya)
+    writer.flush()
+    writer.close()
 
    }
+
    //将数据写进mapafter中
-   val line = arraya.split("\n")
+    val line = arraya.split("\n")
    for (i <- 0 until line.length - 1) {
-   val keyValue = line(i).trim
-   val r = keyValue.split("\t")
-   if (arraya.length > 0) {
+
+     val keyValue = line(i).trim
+     val r = keyValue.split("\t")
+     if (arraya.length > 0) {
    for (x <- 0 until r.length - 1) {
 
-     mapAfter.+=((r(0), r(1).toInt))
+      mapAfter.+=((r(0), r(1).toInt))
+
      }
      }
      }
 
    try {
 
-   //从本地文件读取上个小时的数据 并将结果写入map集合中
-   // 根据时间戳作为上一小时的文件名
-   val databefore = new Date(new Date().getTime - 60 * 60 * 1000)
-   val sdf = new SimpleDateFormat("yyyy-MM-dd-HH")
-   val strbefore = sdf.format(databefore)
-   val fbefore = new File("/home/shj/" + strbefore)
+     //从本地文件读取上个小时的数据 并将结果写入map集合中
+    // 根据时间戳作为上一小时的文件名
+    val databefore = new Date(new Date().getTime - 60 * 60 * 1000)
+    val sdf = new SimpleDateFormat("yyyy-MM-dd-HH")
+    val strbefore = sdf.format(databefore)
+    val fbefore = new File("/home/shj/" + strbefore)
    if(fbefore.exists()) {
 
-    Source.fromFile(fbefore,"UTF-8").getLines().foreach(keyValue => {
+      Source.fromFile(fbefore,"UTF-8").getLines().foreach(keyValue => {
 
-    val r = keyValue.split("\t")
-    mapBefore.+=((r(0), r(1).toInt))
+      val r = keyValue.split("\t")
+      mapBefore.+=((r(0), r(1).toInt))
 
-    })
-   }else{
+     })
+    }else{
 
-   fbefore.createNewFile()
-   println("上一小时无数据")
+      fbefore.createNewFile()
+      println("上一小时无数据")
      }
-   //将最后处理的排序差结果放到mapDiff集合中
-   val mapiterator = mapAfter.iterator
-   var difference = 0
-   var value: Int = 0
-   var key = ""
+    //将最后处理的排序差结果放到mapDiff集合中
+    val mapiterator = mapAfter.iterator
+    var difference = 0
+    var value: Int = 0
+    var key = ""
 
    while (mapiterator.hasNext) {
 
-   val keyWord = mapiterator.next()
-   key = keyWord._1
-   value = keyWord._2
+     val keyWord = mapiterator.next()
+     key = keyWord._1
+     value = keyWord._2
    //判断前后两小时的数据还有有交融的
    if (mapBefore.contains(key)) {
 
-   difference = value - mapBefore.get(key).get
-   mapDiff.+=((key, difference))
-   } else {
+     difference = value - mapBefore.get(key).get
+     mapDiff.+=((key, difference))
 
-   val differences = value - mapBefore.size
-   mapDiff.+=((key, differences))
+    } else {
+
+     val differences = value - mapBefore.size
+     mapDiff.+=((key, differences))
 
    }
+
    //遍历mapDiff 并将keydiff封装到arrKey数组中
    var listKey = new ListBuffer[String]()
    //输出排序差在前100的数据
-   sparkContext.parallelize(mapDiff.toSeq).map(x => (x._2, x._1)).sortByKey(true).takeOrdered(100).foreach(x=>{
+    sparkContext.parallelize(mapDiff.toSeq).map(x => (x._2, x._1)).sortByKey(true).takeOrdered(100).foreach(x=>{
 
-   listKey+=x._2
+    listKey+=x._2
 
     })
+ 
     //getIndustry_words
     getIndustry_words(listKey)
 
@@ -159,38 +169,41 @@ import scala.io.Source
 
     val filestockk=new File("/home/shj/stock.txt")
     Source.fromFile(filestockk, "UTF-8").getLines().foreach(keyValuestock => {
-    val arr = keyValuestock.split("\t")
-    val r1= arr(0)
-    val name = arr(1)
-    val code =r1.split("\\.")(0)
-    mapstockk.+=(name-> code)
+
+      val arr = keyValuestock.split("\t")
+      val r1= arr(0)
+      val name = arr(1)
+      val code =r1.split("\\.")(0)
+      mapstockk.+=(name-> code)
 
     })
     val fileimp2 = new File("/home/shj/stock_words.words")
     // println("fileimp2:"+fileimp)
     Source.fromFile(fileimp2, "UTF-8").getLines().foreach(keyValueimp => {
 
-    val r = keyValueimp.split("\t")
-    val keyimp = r(0)
-    val valueimp = r(1).split(",")
-    mapimp.+=(keyimp -> valueimp)
-    val arrKey = listKey.toArray
-    val arr =arrKey.iterator
+      val r = keyValueimp.split("\t")
+      val keyimp = r(0)
+      val valueimp = r(1).split(",")
+      mapimp.+=(keyimp -> valueimp)
+      val arrKey = listKey.toArray
+      val arr =arrKey.iterator
     while(arr.hasNext) {
-    val arrkey = arr.next()
 
-    //判断 valueimp中是否包含arrKey
+      val arrkey = arr.next()
+
+      //判断 valueimp中是否包含arrKey
     if (valueimp.contains(arrkey)) {
 
-    mapstock_code.+=(arrkey->mapstockk.get(keyimp).get)
+      mapstock_code.+=(arrkey->mapstockk.get(keyimp).get)
 
-    } else {
-    }
-     }
-    })
-     }
-    } catch {
-    case e: Exception => e.printStackTrace()
+     } else {
+      }
+       }
+       })
+         }
+          } catch {
+
+        case e: Exception => e.printStackTrace()
         sparkContext.stop()
     }
 
@@ -204,66 +217,77 @@ import scala.io.Source
     println("____________________")
     println("_____________________________________THE END")
 
-    sparkContext.stop()
-    System.exit(0)
+     sparkContext.stop()
+     System.exit(0)
      }
 
     def compare(): Unit = {
 
-    var paramaMap = new mutable.HashMap[String, String]
-    var paramaMapp = new mutable.HashMap[String, String]
+      var paramaMap = new mutable.HashMap[String, String]
+      var paramaMapp = new mutable.HashMap[String, String]
 
     mapindustry.foreach(x => {
-    val hot_word = x._1
-    val industry = x._2
+      val hot_word = x._1
+      val industry = x._2
 
     if (hot_word != null) {
 
-    paramaMap.+=("hot_words" -> hot_word)
+      paramaMap.+=("hot_words" -> hot_word)
+
       }
     paramaMap.+=("hot_words" -> hot_word, "industry" -> industry)
     HotWordHttp.sendNew("http://120.55.189.211/cgi-bin/northsea/prsim/subscribe/1/hot_words_notice.fcgi", paramaMap)
     if (industry != null) {
 
-    paramaMap.+=("industry" -> industry)
+      paramaMap.+=("industry" -> industry)
+
     }
     paramaMap.+=("hot_words" -> hot_word, "industry" -> industry)
     HotWordHttp.sendNew("http://120.55.189.211/cgi-bin/northsea/prsim/subscribe/1/hot_words_notice.fcgi", paramaMap)
+
     })
 
     mapsection.foreach(x => {
 
-    val hot_word = x._1
-    val section = x._2
+      val hot_word = x._1
+      val section = x._2
 
     if (hot_word != null) {
-    paramaMap.+=("hot_words" -> hot_word)
+
+      paramaMap.+=("hot_words" -> hot_word)
+
      }
     paramaMap.+=("hot_words" -> hot_word, "section" -> section)
     HotWordHttp.sendNew("http://120.55.189.211/cgi-bin/northsea/prsim/subscribe/1/hot_words_notice.fcgi", paramaMap)
     if (section != null) {
 
     paramaMap.+=("section" -> section)
+
     }
     paramaMap.+=("hot_words" -> hot_word, "section" -> section)
     HotWordHttp.sendNew("http://120.55.189.211/cgi-bin/northsea/prsim/subscribe/1/hot_words_notice.fcgi", paramaMap)
     })
 
     mapstock_code.foreach(x => {
-    val hot_word = x._1
-    val stock_code = x._2
+
+      val hot_word = x._1
+      val stock_code = x._2
 
     if (hot_word != null) {
-    paramaMapp.+=("hot_words" -> hot_word)
+
+      paramaMapp.+=("hot_words" -> hot_word)
+
     }
     paramaMapp.+=("hot_words" -> hot_word, "stock_code" -> stock_code)
     HotWordHttp.sendNew("http://120.55.189.211/cgi-bin/northsea/prsim/subscribe/1/hot_words_notice.fcgi", paramaMapp)
     if (stock_code != null) {
 
-    paramaMapp.+=("stock_code" -> stock_code)
+      paramaMapp.+=("stock_code" -> stock_code)
+
     }
     paramaMapp.+=("hot_words" -> hot_word, "stock_code" -> stock_code)
     HotWordHttp.sendNew("http://120.55.189.211/cgi-bin/northsea/prsim/subscribe/1/hot_words_notice.fcgi", paramaMapp)
+
     })
     }
 
@@ -272,17 +296,18 @@ import scala.io.Source
      **/
     def SplitWords(tuple: (ImmutableBytesWritable, Result)): mutable.MutableList[String] = {
 
-    var list = new mutable.MutableList[String]
-    val result = tuple._2
-    val title = Bytes.toString(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("title")))
-    val reader: StringReader = new StringReader(title)
-    val ik = new IKSegmentation(reader, true)
-    var lexeme = ik.next()
+      var list = new mutable.MutableList[String]
+      val result = tuple._2
+      val title = Bytes.toString(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("title")))
+      val reader: StringReader = new StringReader(title)
+      val ik = new IKSegmentation(reader, true)
+      var lexeme = ik.next()
     while (lexeme != null) {
 
-    val word = lexeme.getLexemeText()
-    list += word.toString
-    lexeme = ik.next()
+      val word = lexeme.getLexemeText()
+      list += word.toString
+      lexeme = ik.next()
+
     }
 
     list
@@ -291,98 +316,101 @@ import scala.io.Source
     //获取时间戳
     def getTime(): Unit ={
 
-    val scan = new Scan()
-    val date = new Date(new Date().getTime - 60 * 60 * 1000)
-    val format = new SimpleDateFormat("yyyy-MM-dd HH")
-    val time = format.format(date)
-    val time1 = format.format(new Date().getTime)
-    val starttime = time + "-00-00"
-    val stoptime = time1 + "-00-00"
-    val sdf1: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss")
-    val startrow: Long = sdf1.parse(starttime).getTime
-    val stoprow: Long = sdf1.parse(stoptime).getTime
-    println(startrow)
-    println(stoprow)
-    scan.setTimeRange(startrow, stoprow)
-    val proto: ClientProtos.Scan = ProtobufUtil.toScan(scan)
-    val scanToString = Base64.encodeBytes(proto.toByteArray)
-    conf.set(TableInputFormat.SCAN, scanToString)
+      val scan = new Scan()
+      val date = new Date(new Date().getTime - 60 * 60 * 1000)
+      val format = new SimpleDateFormat("yyyy-MM-dd HH")
+      val time = format.format(date)
+      val time1 = format.format(new Date().getTime)
+      val starttime = time + "-00-00"
+      val stoptime = time1 + "-00-00"
+      val sdf1: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss")
+      val startrow: Long = sdf1.parse(starttime).getTime
+      val stoprow: Long = sdf1.parse(stoptime).getTime
+      println(startrow)
+      println(stoprow)
+      scan.setTimeRange(startrow, stoprow)
+      val proto: ClientProtos.Scan = ProtobufUtil.toScan(scan)
+      val scanToString = Base64.encodeBytes(proto.toByteArray)
+      conf.set(TableInputFormat.SCAN, scanToString)
+
      }
 
     //从本地获取行业关键字词典 并放入mapimp集合中
     def getIndustry_words(listKey:ListBuffer[String]): Unit ={
-    val arrKey = listKey.toArray
-    val fileimp1 = new File("/home/shj/industry_words1.words")
-    //println("fileimp1:"+fileimp)
+      val arrKey = listKey.toArray
+      val fileimp1 = new File("/home/shj/industry_words1.words")
+      //println("fileimp1:"+fileimp)
     Source.fromFile(fileimp1, "UTF-8").getLines().foreach(keyValueimp => {
 
-    val r = keyValueimp.split("\t")
-    val keyimp = r(0)
-    val valueimp = r(1).split(",")
-    mapimp.+=(keyimp -> valueimp)
+      val r = keyValueimp.split("\t")
+      val keyimp = r(0)
+      val valueimp = r(1).split(",")
+      mapimp.+=(keyimp -> valueimp)
 
-    val arr =arrKey.iterator
+      val arr =arrKey.iterator
     while(arr.hasNext) {
 
-    val arrkey = arr.next()
+      val arrkey = arr.next()
     //判断 valueimp中是否包含arrKey
     if (valueimp.contains(arrkey)) {
 
-    mapsection.+=(arrkey -> keyimp)
+       mapsection.+=(arrkey -> keyimp)
 
-    } else {
-
+      } else {
+ 
     }
-    }
-    })
-    }
+     }
+      })
+        }
 
     //从本地获取板块关键字词典 并放入mapimp集合中
     def getsection_words(listKey:ListBuffer[String]): Unit ={
 
-    val arrKey = listKey.toArray
-    val fileimp1 = new File("/home/shj/section_words.words")
-    //println("fileimp1:"+fileimp)
+      val arrKey = listKey.toArray
+      val fileimp1 = new File("/home/shj/section_words.words")
+      //println("fileimp1:"+fileimp)
     Source.fromFile(fileimp1, "UTF-8").getLines().foreach(keyValueimp => {
 
-    val r = keyValueimp.split("\t")
-    val keyimp = r(0)
-    val valueimp = r(1).split(",")
-    mapimp.+=(keyimp -> valueimp)
+      val r = keyValueimp.split("\t")
+      val keyimp = r(0)
+      val valueimp = r(1).split(",")
+      mapimp.+=(keyimp -> valueimp)
 
-    val arr =arrKey.iterator
+      val arr =arrKey.iterator
     while(arr.hasNext) {
-    val arrkey = arr.next()
+      val arrkey = arr.next()
 
     //判断 valueimp中是否包含arrKey
     if (valueimp.contains(arrkey)) {
 
-    mapsection.+=(arrkey -> keyimp)
+      mapsection.+=(arrkey -> keyimp)
 
-    } else {
+     } else {
 
-    }
-     }
-    })
-    }
+      }
+      }
+      })
+       }
 
     def init(tb:String): Unit ={
 
-    conf.set("hbase.rootdir", "hdfs://ns1/hbase")
-    conf.set("hbase.zookeeper.quorum", "server0,server1,server2")
-    conf.set(TableInputFormat.INPUT_TABLE, tb)
+      conf.set("hbase.rootdir", "hdfs://ns1/hbase")
+      conf.set("hbase.zookeeper.quorum", "server0,server1,server2")
+      conf.set(TableInputFormat.INPUT_TABLE, tb)
     //    conf.set(TableInputFormat.INPUT_TABLE, "3_analyzed")
     //    conf.set(TableInputFormat.INPUT_TABLE, "6_analyzed")
-    conf.set(TableInputFormat.SCAN_COLUMN_FAMILY, "info")
-    conf.set(TableInputFormat.SCAN_COLUMNS, "title")
+      conf.set(TableInputFormat.SCAN_COLUMN_FAMILY, "info")
+      conf.set(TableInputFormat.SCAN_COLUMNS, "title")
 
-    System.setProperty("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      System.setProperty("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+
     }
 
 
     def main(args: Array[String]) {
 
-    obtainHbaseRdd()
+      obtainHbaseRdd()
+
     }
     }
 
