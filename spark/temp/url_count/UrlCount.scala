c14927d354c87148df7d2607c6bac08f6ce9b4ed
liaochengming <liaochengming@kunyan-inc.com>
2016-04-21 18:21:01 +0800

    url访问次数统计

---
+++ spark/temp/url_count/UrlCount.scala
@@ -0,0 +1,42 @@
+import java.io.{File, PrintWriter}
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+ * Created by lcm on 2016/4/21.
+ */
+object UrlCount {
+
+  val url_count = new scala.collection.mutable.ListBuffer[String]()
+
+  def main(args: Array[String]) {
+    val in = args(0)
+    val out = args(1)
+    val sparkConf = new SparkConf().setAppName("FILTER AD").setMaster("local")
+    val sc = new SparkContext(sparkConf)
+
+    val writerUrlCount = new PrintWriter(new File(out), "UTF-8")
+
+    sc.textFile(in).map(x => x.split("\t")).filter(_.length > 4)
+      .filter(_ (3).contains("/"))
+      .map(x => {
+        val url = x(3)
+        if (url.contains("http://") && url.split("/").length > 3) {
+          (url.split("/")(2), 1)
+        } else {
+          (url.split("/")(0), 1)
+        }
+      })
+      .reduceByKey(_ + _)
+      .map(x => (x._2, x._1))
+      .sortByKey(false)
+      .map(x => (x._2 + "\t" + x._1))
+      .foreach(x => {
+        url_count.+=(x)
+      })
+    for (x <- url_count) {
+      writerUrlCount.write(x + "\n")
+    }
+    writerUrlCount.close()
+  }
+}
