87d15426a521cf8b172c4950a8cdd937772193b4
liaochengming <liaochengming@kunyan-inc.com>
2016-04-25 10:55:17 +0800

    修改了http的判断，修改了代码集合的写法，修改了保存方式

--- spark/temp/url_count/UrlCount.scala
+++ spark/temp/url_count/UrlCount.scala
@@ -1,5 +1,8 @@
+import _root_.java.io.File
+import _root_.java.io.PrintWriter
 import java.io.{File, PrintWriter}
 
+import _root_.org.apache.spark.SparkConf
 import org.apache.spark.{SparkContext, SparkConf}
 
 /**
@@ -8,36 +11,26 @@ import org.apache.spark.{SparkContext, SparkConf}
  */
 object UrlCount {
 
-  val url_count = new scala.collection.mutable.ListBuffer[String]()
-
   def main(args: Array[String]) {
     val in = args(0)
     val out = args(1)
     val sparkConf = new SparkConf().setAppName("FILTER AD")
     val sc = new SparkContext(sparkConf)
 
-    val writerUrlCount = new PrintWriter(new File(out), "UTF-8")
-
     sc.textFile(in).map(x => x.split("\t")).filter(_.length > 4)
       .filter(_ (3).contains("/"))
       .map(x => {
         val url = x(3)
-        if (url.contains("http://") && url.split("/").length > 3) {
-          (url.split("/")(2), 1)
+        if (url.startsWith("http") && url.split("/").length > 2) {
+          (url.split("/")(2),1)
         } else {
-          (url.split("/")(0), 1)
+          (url.split("/")(0),1)
         }
       })
       .reduceByKey(_ + _)
       .map(x => (x._2, x._1))
       .sortByKey(false)
       .map(x => (x._2 + "\t" + x._1))
-      .foreach(x => {
-        url_count.+=(x)
-      })
-    for (x <- url_count) {
-      writerUrlCount.write(x + "\n")
-    }
-    writerUrlCount.close()
+      .saveAsTextFile(out)
   }
 }
