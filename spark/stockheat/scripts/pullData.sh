6981a7a63f6120a32e774447004f6d9303afa3fc
sunsolo <wukun@kunyan-inc.com>
2016-07-18 19:59:12 +0800

    修改搜索补跑程序

--- spark/stockheat/scripts/pullData.sh
+++ spark/stockheat/scripts/pullData.sh
@@ -1,33 +1,71 @@
 #!/usr/bin/env bash
 
 ZERO=0
-FOUR=3
+FOUR=2
 
 HALF=$(date +%H)
 DAY=$(date +%d)
 
-if [ $HALF -ge $ZERO ] && [ $HALF -le $FOUR ];then
-  HALF=$((10#$HALF + 24))
-  HALF=$((10#$HALF - 4))
-  DAY=$((10#$DAY - 1))
-  if [ $DAY -ge 1 ] && [ $DAY -le 9 ]; then
-    HALF=$(date +%Y%m)0${DAY}$HALF
+FILENAME=""
+
+#通过当前时间戳计算应当出现的文件的文件名
+function computeFileName() {
+
+  if [ $HALF -ge $ZERO ] && [ $HALF -le $FOUR ];then
+    HALF=$((10#$HALF + 24))
+    HALF=$((10#$HALF - 3))
+    DAY=$((10#$DAY - 1))
+    if [ $DAY -ge 1 ] && [ $DAY -le 9 ]; then
+      HALF=$(date +%Y%m)0${DAY}$HALF
+    else 
+      HALF=$(date +%Y%m)${DAY}$HALF
+    fi
   else 
-    HALF=$(date +%Y%m)${DAY}$HALF
+    HALF=$((10#$HALF - 3))
+    if [ $HALF -ge $ZERO ] && [ $HALF -le 9 ];then
+      HALF=$(date +%Y%m%d)0$HALF
+    else 
+      HALF=$(date +%Y%m%d)$HALF
+    fi
   fi
-else 
-  HALF=$((10#$HALF - 4))
-  if [ $HALF -ge $ZERO ] && [ $HALF -le 9 ];then
-    HALF=$(date +%Y%m%d)0$HALF
-  else 
-    HALF=$(date +%Y%m%d)$HALF
+
+  FILENAME="shdx_201607181"
+  #FILENAME="shdx_"$HALF
+}
+
+#远程从30主机拉取需要的文件，并上传到hdfs
+function scpFile() {
+
+  scp wukun@192.168.1.30:/home/telecom/shdx/data/search/$1 /home/wukun/work/Wokong/src/main/scala/com/kunyan/wokongsvc/realtimedata/data
+
+  if [ $? -eq 0 ];then
+    sendFileToHdfs $1
+    echo $1 >> hold_backup.txt
+  else
+    echo $1 >> lose_backup.txt
   fi
-fi
+}
 
-echo $HALF
-echo $DAY
+#上传到hdfs
+function sendFileToHdfs() {
 
-FILENAME="shdx_"$HALF
+  /home/hadoop/hadoop-2.7.1/bin/hdfs dfs -put /home/wukun/work/Wokong/src/main/scala/com/kunyan/wokongsvc/realtimedata/data/$1 /user/wukun/shdx
+  rm -f /home/wukun/work/Wokong/src/main/scala/com/kunyan/wokongsvc/realtimedata/data/$1
+}
 
-scp wukun@222.73.34.97:/home/dx_datafile/shdx_2016061412 /home/wukun/work/Wokong/src/main/scala/com/kunyan/wokongsvc/realtimedata/data
+computeFileName
+scpFile $FILENAME
+
+cat ./lose.txt | while read line
+do 
+  scpFile $line
+done
+
+cat lose_backup.txt > lose.txt
+cat hold_backup.txt > hold.txt
+
+echo $FILENAME
+
+: > hold_backup.txt
+: > lose_backup.txt
 
