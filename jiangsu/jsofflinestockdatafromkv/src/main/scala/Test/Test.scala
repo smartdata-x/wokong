cecbb7718d691e101788fe37bf8288e657a4defc
shanghai-Jerry <cjyou20110920sspu@163.com>
2016-09-05 17:57:32 +0800

    + kv取值key的变化 0 -> 00 5 -> 05

--- jiangsu/jsofflinestockdatafromkv/src/main/scala/Test/Test.scala
+++ jiangsu/jsofflinestockdatafromkv/src/main/scala/Test/Test.scala
@@ -2,6 +2,8 @@ package Test
 
 import java.util.{Calendar, Date, Timer}
 
+import config.FileConfig
+import log.UserLogger
 import timer.MyTimerTask
 
 /**
@@ -14,7 +16,7 @@ object Test {
 
   def main(args: Array[String]) {
 
-    /*FileConfig.DATA_DIR = args(0)
+    FileConfig.DATA_DIR = args(0)
     FileConfig.LOG_DIR = args(1)
     FileConfig.PROGRESS_DIR = args(2)
     UserLogger.logConfigureFile(args(3))
@@ -24,9 +26,9 @@ object Test {
 
     // XMLConfig.apply("E:\\jsdxkvdown.xml")
 
-    val task = new MyTimerTask(args(6).toInt, startExecutorTask, endExecutorTask)*/
+    val task = new MyTimerTask(args(6).toInt, startExecutorTask, endExecutorTask)
 
-    val task = new MyTimerTask( -10,0,1)
+    // val task = new MyTimerTask( -10,0,1)
 
     task.run()
     val timer = new Timer()
